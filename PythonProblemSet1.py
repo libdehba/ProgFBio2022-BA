b0686e08dce7d1038aab1a0b28c1b535f73e463a
student <student@info-17.cshl.edu>
2022-10-11 11:21:11 -0400

    PythonProbSet1

---
+++ PythonProblemSet1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+name = sys.argv[1]
+color = sys.argv[2]
+FavoriteActivity = sys.argv[3]
+FavAnimal = sys.argv[4] 
+print('My name is', name)
+print('My favorite color is',color)
+print('My favorite activity is',FavoriteActivity)
+print('My favorite animal is a',FavAnimal)
+
+
