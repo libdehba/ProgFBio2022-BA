668f2492145e5f912de6fb2dfb1782a124b57f44
student <student@info-17.cshl.edu>
2022-10-18 21:15:48 -0400

    Brian Haas Workshop Updated

--- fastq_file_to_sequence_list.py
+++ fastq_file_to_sequence_list.py
@@ -20,7 +20,7 @@ import os, sys, re
 
 
 def seq_list_from_fastq_file(fastq_filename):
-
+:w
     seq_list = list()
 
 
@@ -44,7 +44,7 @@ def sequence_to_kmer_list(sequence, kmer_length):
     count = 0
     kmer_list = [] 
     for nt_position in range(0 , (len(sequence)-kmer_length +1)):
-      print(nt_position)   
+ #     print(nt_position)   
       kmer_list.append(sequence[nt_position:nt_position + kmer_length])
 
 
@@ -54,12 +54,19 @@ def sequence_to_kmer_list(sequence, kmer_length):
 
 seq_list_needed = seq_list_from_fastq_file('reads.fq')
 for seq in seq_list_needed:
-  print(sequence_to_kmer_list(seq, 5))
+#  print(sequence_to_kmer_list(seq, 5))
+
+  kmer_list2 = sequence_to_kmer_list(seq, 5)
+  kmer_list_organized = kmer_list2[0:1]
+  print(kmer_list_organized.sort())i
+#need to add a for loop to iterate through my kmers list
+#  print(kmer_list2)
 
 
 
-  sorted = list.sort(sequence_to_kmer_list(seq, 5))
-  print(sorted)
+
+ # sorted = list.sort(sequence_to_kmer_list(seq, 5))
+ # print(sorted)
 
 
 #print(sequence_to_kmer_list('ATGTTTACGTAGCTAGTAGATGATGA', 5))
@@ -99,3 +106,11 @@ for seq in seq_list_needed:
 
 #if __name__ == '__main__':
 #    main() 
+
+
+#_______________________________ Brian's Answers___:
+#First Challenge: same as above
+#2nd channgele:see about
+#3rd part: 
+#combining the first 2 parts: count the kamers
+#part 1 of this
